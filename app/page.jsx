76137f36095381c56ce0ce8f7f729d818f16b289
Aestheticsuraj234 <jhapriya458@gmail.com>
2023-06-22 03:52:10 +0530

    Awesome

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+import Feed from "@components/feed"
+
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">
+            Discover & share
+            <br className="max-md:hidden"/>
+            <span className="orange_gradient text-center">AI-Powered Promtps</span>
+            <p className="desc text-center">Promtopia is an open-source AI prompting tool for modern world to discover , create and share creative prompts</p>
+            <Feed/>
+            </h1>
+    </section>
+  )
+}
+
+export default Home
---
+++ components/Feed.jsx
@@ -0,0 +1,98 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
+
+const Feed = () => {
+  const [allPosts, setAllPosts] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
+
+
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch("/api/prompt");
+        console.log(response)
+        if (!response.ok) {
+          throw new Error("Failed to fetch prompt data");
+        }
+        const data = await response.json();
+        setAllPosts(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchPosts();
+  }, []);
+
+  const filterPrompts = (searchText) => {
+    const regex = new RegExp(searchText, "i");
+    return allPosts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
+
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or a username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+
+      {searchText ? (
+        <PromptCardList
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PromptCardList data={allPosts} handleTagClick={handleTagClick} />
+      )}
+    </section>
+  );
+};
+
+export default Feed;
